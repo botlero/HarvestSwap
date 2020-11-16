9997e3f7a54442639deceb4eda8e1c250f4f2e55
botlero <71649923+botlero@users.noreply.github.com>
2020-11-15 22:38:02 -0500

    Add files via upload

---
+++ DDL/HarvestSwap Inserts.sql
@@ -0,0 +1,22 @@
+INSERT INTO gardener (gardener_id,gardener_user_name,first_name,last_name,gardener_address,email,gardener_image,gardener_bio,gardener_password)
+VALUES (1,'botlero777','Lucinda','Botlero','123 Main Street Elkridge MD-21075','botlero@gmail.com','https://ischool.umd.edu/about/directory/lucinda-botlero','Hi, I am Lucinda','botlero123');
+INSERT INTO gardener (gardener_id,gardener_user_name,first_name,last_name,gardener_address,email,gardener_image,gardener_bio,gardener_password)
+VALUES (2,'gaurav','Gaurav','Hasija','555 Dobbin Road, Columbia, MD-21045','hasija@gmail.com','','Hi, I am Gaurav','hasija123');
+INSERT INTO gardener (gardener_id,gardener_user_name,first_name,last_name,gardener_address,email,gardener_image,gardener_bio,gardener_password)
+VALUES (3,'amar','Amar','Javaid','675 Troy Hills Drive, Catonsville, MD-21044','javaid@gmail.com','','Hi, I am Amar','javaid123');
+INSERT INTO gardener (gardener_id,gardener_user_name,first_name,last_name,gardener_address,email,gardener_image,gardener_bio,gardener_password)
+VALUES (4,'kayla','Kayla','Covington','414 Center Park Drive, Laurel, MD-21039','coington@gmail.com','','Hi, I am Kayla','covington123');
+INSERT INTO gardener (gardener_id,gardener_user_name,first_name,last_name,gardener_address,email,gardener_image,gardener_bio,gardener_password)
+VALUES (5,'devin','Devin','Sparrow','8926 Dorsey Rd, Ellicot City, MD-21025','sparrow@gmail.com','','Hi, I am Dvin','sparrow123');
+
+
+INSERT INTO harvest (gardener_id,harvest_name,harvest_date,harvest_qty,harvest_description,harvest_image)
+VALUES(1,'tomato','20201115','5lbs','Organically grown very sweet cherry tomatos','https://www.hometownseeds.com/products/large-red-cherry-tomato-seeds');
+INSERT INTO harvest (gardener_id,harvest_name,harvest_date,harvest_qty,harvest_description,harvest_image)
+VALUES(2,'pepper','20201116','2 lbs','Spicy Thai birds eye pepper','');
+INSERT INTO harvest (gardener_id,harvest_name,harvest_date,harvest_qty,harvest_description,harvest_image)
+VALUES(3,'squash','20201117','5 lbs','Smei sweet garden suqsh. Great for grilling','');
+INSERT INTO harvest (gardener_id,harvest_name,harvest_date,harvest_qty,harvest_description,harvest_image)
+VALUES(4,'zucchini','20201118','1lbs','green zucchinni','');
+INSERT INTO harvest (gardener_id,harvest_name,harvest_date,harvest_qty,harvest_description,harvest_image)
+VALUES(5,'eggplant','20201119','2 lbs','Chinese eggplant which is great for asian dishes','');
