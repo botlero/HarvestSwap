f0af985b68bd7f568e8cee15ef45b58d9850ef1e
botlero <71649923+botlero@users.noreply.github.com>
2020-11-21 12:45:17 -0500

    Update HarvestSwap Inserts.sql

--- DDL/HarvestSwap Inserts.sql
+++ DDL/HarvestSwap Inserts.sql
@@ -1,13 +1,13 @@
 INSERT INTO gardener (gardener_id,gardener_user_name,first_name,last_name,gardener_address,email,gardener_image,gardener_bio,gardener_password)
 VALUES (1,'botlero777','Lucinda','Botlero','123 Main Street Elkridge MD-21075','botlero@gmail.com','https://ischool.umd.edu/about/directory/lucinda-botlero','Hi, I am Lucinda','botlero123');
 INSERT INTO gardener (gardener_id,gardener_user_name,first_name,last_name,gardener_address,email,gardener_image,gardener_bio,gardener_password)
-VALUES (2,'gaurav','Gaurav','Hasija','555 Dobbin Road, Columbia, MD-21045','hasija@gmail.com','','Hi, I am Gaurav','hasija123');
+VALUES (2,'gaurav111','Gaurav','Hasija','555 Dobbin Road, Columbia, MD-21045','hasija@gmail.com','','Hi, I am Gaurav','hasija123');
 INSERT INTO gardener (gardener_id,gardener_user_name,first_name,last_name,gardener_address,email,gardener_image,gardener_bio,gardener_password)
-VALUES (3,'amar','Amar','Javaid','675 Troy Hills Drive, Catonsville, MD-21044','javaid@gmail.com','','Hi, I am Amar','javaid123');
+VALUES (3,'amar999','Amar','Javaid','675 Troy Hills Drive, Catonsville, MD-21044','javaid@gmail.com','','Hi, I am Amar','javaid123');
 INSERT INTO gardener (gardener_id,gardener_user_name,first_name,last_name,gardener_address,email,gardener_image,gardener_bio,gardener_password)
-VALUES (4,'kayla','Kayla','Covington','414 Center Park Drive, Laurel, MD-21039','covington@gmail.com','','Hi, I am Kayla','covington123');
+VALUES (4,'kayla333','Kayla','Covington','414 Center Park Drive, Laurel, MD-21039','covington@gmail.com','','Hi, I am Kayla','covington123');
 INSERT INTO gardener (gardener_id,gardener_user_name,first_name,last_name,gardener_address,email,gardener_image,gardener_bio,gardener_password)
-VALUES (5,'devin','Devin','Sparrow','8926 Dorsey Rd, Ellicot City, MD-21025','sparrow@gmail.com','','Hi, I am Devin','sparrow123');
+VALUES (5,'devin555','Devin','Sparrow','8926 Dorsey Rd, Ellicot City, MD-21025','sparrow@gmail.com','','Hi, I am Devin','sparrow123');
 
 
 INSERT INTO harvest (gardener_id,harvest_name,harvest_date,harvest_qty,harvest_description,harvest_image)
