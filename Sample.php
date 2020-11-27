90fb48101393ff9129e32c8bc59b428b0b4a5ec8
botlero <lbotlero@umd.edu>
2020-11-26 22:52:12 -0500

    Adding Header and Footer PHP pages.
    A sample is added just to show where the code should go in every page.

---
+++ Sample.php
@@ -0,0 +1,18 @@
+
+<!--USE THIS CODE ON EVERY PAGE. COPY & PASTE LINES 5-7 ON TOP OF YOUR CODE ; COPY & PASTE LINES 16-18 ON BOTTOM OF YOUR CODE-->
+
+
+<?php
+        include('includes/Header.php');
+?>
+
+
+  <p  style="height:200px">
+ YOUR CODE GOES HERE!
+</p>
+</p>
+
+
+<?php
+        include('includes/Footer.php');
+?>
