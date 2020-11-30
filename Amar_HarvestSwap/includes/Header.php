490ec4d29cf297c4f4c0315560d94ade66b05d1e
ajavaid24 <74329329+ajavaid24@users.noreply.github.com>
2020-11-29 21:43:41 -0500

    Add files via upload

---
+++ Amar_HarvestSwap/includes/Header.php
@@ -0,0 +1,30 @@
+<!--DO NOT CHANGE THIS CODE-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<title>HarvestSwap</title>
+</head>
+
+<body>
+	<header>
+		<div class="header">
+   			<img src="images/harvet-swap-logo-big.png" alt="Fresh Produce">
+			<h2>Home Grown Fresh Produce, Swap With A Neighbor!</h2>
+		</div>
+<div class="navbar">
+  <a class="active" href="#"><i class="fas fa-home"></i>Welcome</a> 
+  <a href="#"><i class="fa fa-fw fa-info"></i> How It Works</a> 
+  <a href="#"><i class="fas fa-people-arrows"></i> SwapCenter</a> 
+  <a href="#"><i class="fa fa-fw fa-user"></i> Create Account</a>
+  <a href="#"><i class="fa fa-fw fa-address-card"></i> My Profile</a>
+  <a href="#"><i class="fa fa-fw fa-key"></i> Login</a>
+</div>
+</header>
+
+
+
+    
